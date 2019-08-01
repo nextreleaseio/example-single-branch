9b06b38e9a8901e8a3f7f0983bbef8c98daa187c
Donald Wasserman <djwasserman@gmail.com>
2019-08-01 16:56:01 -0400

    More cpp

--- sample.cpp
+++ sample.cpp
@@ -5,4 +5,12 @@ int main ()
 {
   cout << "Hello World! ";
   cout << "I'm a C++ program";
+  cout << "C++ sure isn't phone";
+  cout << "stupid really";
+  other()
+}
+
+int other () 
+{
+  cout << "I bet this won't even compile";
 }
