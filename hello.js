c7efa14df930e450d9451ee8997b914ce8cc1f57
Donald Wasserman <djwasserman@gmail.com>
2019-08-02 13:04:55 -0400

    Updates

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
 console.log('hello')
+
+class MyClass {
+  myField = 1;
+  constructor() {
+   console.log('I need more syntax highligthing in vim');
+ }
+
+  mungeData(data) {
+    return this.myField + data;
+  }
+}
