6b9ee8d4872983fa48db9961d24f5109b442a320
Donald Wasserman <djwasserman@gmail.com>
2019-08-06 13:45:09 -0400

    Added some rust

--- hello.rs
+++ hello.rs
@@ -4,5 +4,15 @@ fn main() {
 
     // Print text to the console
     println!("Hello World!");
+    let mut x = 10;
+    let mut done = false;
+
+    while !done {
+        x += x - 3;
+        println("X is {}", x);
+
+        if x % 7 == 0 {
+            done = true;
+        }
 }
 
